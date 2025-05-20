5eae8522dd0402675915ba32e41891f6680a032b
keypang <bestnest820@naver.com>
2025-05-21 02:22:36 +0900

    [#7] feat: 미션 추가

--- umc_spring_mission/src/main/java/com/example/umc_spring_mission/web/controller/StoreRestController.java
+++ umc_spring_mission/src/main/java/com/example/umc_spring_mission/web/controller/StoreRestController.java
@@ -3,12 +3,15 @@ package com.example.umc_spring_mission.web.controller;
 import com.example.umc_spring_mission.apiPayload.ApiResponse;
 import com.example.umc_spring_mission.apiPayload.exception.ErrorStatus;
 import com.example.umc_spring_mission.apiPayload.exception.handler.UserHandler;
+import com.example.umc_spring_mission.converter.MissionConverter;
 import com.example.umc_spring_mission.converter.ReviewConverter;
 import com.example.umc_spring_mission.converter.StoreConverter;
+import com.example.umc_spring_mission.domain.Mission;
 import com.example.umc_spring_mission.domain.Restaurant;
 import com.example.umc_spring_mission.domain.Review;
 import com.example.umc_spring_mission.domain.User;
 import com.example.umc_spring_mission.repository.RestaurantRepository.RestaurantRepository;
+import com.example.umc_spring_mission.service.MissionService.MissionCommandService;
 import com.example.umc_spring_mission.service.ReviewService.ReviewCommandService;
 import com.example.umc_spring_mission.service.StoreService.StoreCommandService;
 import com.example.umc_spring_mission.validation.annotation.ExistRestaurant;
@@ -16,6 +19,8 @@ import com.example.umc_spring_mission.web.dto.Review.ReviewRequestDTO;
 import com.example.umc_spring_mission.web.dto.Review.ReviewResponseDTO;
 import com.example.umc_spring_mission.web.dto.Store.StoreRequestDTO;
 import com.example.umc_spring_mission.web.dto.Store.StoreResponseDTO;
+import com.example.umc_spring_mission.web.dto.mission.MissionRequestDTO;
+import com.example.umc_spring_mission.web.dto.mission.MissionResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -29,6 +34,7 @@ public class StoreRestController {
 
     private final StoreCommandService storeCommandService;
     private final ReviewCommandService reviewCommandService;
+    private final MissionCommandService missionCommandService;
 
 
     @PostMapping ("/")
@@ -44,5 +50,13 @@ public class StoreRestController {
         Review review = reviewCommandService.addReview(request,restaurantId);
 
         return ApiResponse.onSuccess(ReviewConverter.toAddReviewResponseDTO(review));
+    }
+
+    @PostMapping("/{restaurantId}/missions")
+    public ApiResponse<MissionResponseDTO.AddMissionResponseDTO>
+    join(@PathVariable @ExistRestaurant @Valid Long restaurantId, @RequestBody @Valid MissionRequestDTO.AddMissionRequestDTO request){
+        Mission mission = missionCommandService.addMission(request, restaurantId);
+
+        return ApiResponse.onSuccess(MissionConverter.toAddMissionResponseDTO(mission));
     }
 }

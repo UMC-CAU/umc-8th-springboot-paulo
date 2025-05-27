fad6f56e95771baa46c93a8928e00fcf124ae8c5
keypang <bestnest820@naver.com>
2025-05-27 15:38:30 +0900

    [#17] feat: 내가 작성한 리뷰 목록 구현

--- umc_spring_mission/src/main/java/com/example/umc_spring_mission/service/ReviewService/ReviewQueryServiceImpl.java
+++ umc_spring_mission/src/main/java/com/example/umc_spring_mission/service/ReviewService/ReviewQueryServiceImpl.java
@@ -2,8 +2,10 @@ package com.example.umc_spring_mission.service.ReviewService;
 
 import com.example.umc_spring_mission.domain.Restaurant;
 import com.example.umc_spring_mission.domain.Review;
+import com.example.umc_spring_mission.domain.User;
 import com.example.umc_spring_mission.repository.RestaurantRepository.RestaurantRepository;
 import com.example.umc_spring_mission.repository.ReviewRepository.ReviewRepository;
+import com.example.umc_spring_mission.repository.UserRepository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +17,7 @@ public class ReviewQueryServiceImpl implements ReviewQueryService {
 
     private final RestaurantRepository restaurantRepository;
     private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Page<Review> getReviewList(Long storeId, Integer page) {
@@ -22,5 +25,13 @@ public class ReviewQueryServiceImpl implements ReviewQueryService {
 
         Page<Review> storePage = reviewRepository.findAllByRestaurant(restaurant, PageRequest.of(page, 10));
         return storePage;
+    }
+
+    @Override
+    public Page<Review> getReviewList(Integer page) {
+        User user = userRepository.findById(1L).get();
+
+        Page<Review> userPage = reviewRepository.findAllByUser(user, PageRequest.of(page, 10));
+        return userPage;
     }
 }
--- umc_spring_mission/src/main/java/com/example/umc_spring_mission/web/controller/UserRestController.java
+++ umc_spring_mission/src/main/java/com/example/umc_spring_mission/web/controller/UserRestController.java
@@ -1,17 +1,19 @@
 package com.example.umc_spring_mission.web.controller;
 
 import com.example.umc_spring_mission.apiPayload.ApiResponse;
+import com.example.umc_spring_mission.converter.ReviewConverter;
 import com.example.umc_spring_mission.converter.UserConverter;
+import com.example.umc_spring_mission.domain.Review;
 import com.example.umc_spring_mission.domain.User;
+import com.example.umc_spring_mission.service.ReviewService.ReviewQueryService;
 import com.example.umc_spring_mission.service.UserService.UserCommandService;
+import com.example.umc_spring_mission.web.dto.Review.ReviewResponseDTO;
 import com.example.umc_spring_mission.web.dto.User.UserRequestDTO;
 import com.example.umc_spring_mission.web.dto.User.UserResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
 
     private final UserCommandService userCommandService;
+    private final ReviewQueryService reviewQueryService;
 
     @PostMapping("/")
     public ApiResponse<UserResponseDTO.JoinResultDTO> join(@RequestBody @Valid UserRequestDTO.JoinDto request){
         User user = userCommandService.joinUser(request);
         return ApiResponse.onSuccess(UserConverter.toJoinResultDTO(user));
+    }
+
+    @PostMapping("/reviews")
+    public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getUserReviewList(@RequestParam(name = "page") Integer page){
+
+        Page<Review> reviewList = reviewQueryService.getReviewList(page);
+        return ApiResponse.onSuccess(ReviewConverter.reviewPreViewListDTO(reviewList));
     }
 }
